3f3b2bd411b95b9fa648bbe39f5d5ef086562c6f
tuckerreiland <treiland@uw.edu>
2022-07-04 11:22:55 -0400

    something's not right

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+
+//navbar generator
+    //ol
+        //li
+            //pop up
+            //articles
+            //videos
+            //store
+            //search
+            //jam button
+
+//global animations
+    //TV animations for sure
+    //like it does an old school channel flip effect between webpages
+    //static
+    //scanlines
+    //I'd like to figure out how to generate a scan line effect over the webpage
+    //logo loads like a couple lines at a time, like a BASIC terminal loading
+    //Windows boot sound (maybe)
+    //old school BASIC style animations for the search bar and stuff
+        //it'd be cool if there was a navbar on page load
+        //choices for the navbar load like one at a time
+        //mouse over gives it the rainbow color background (or black)
+        //search magnifying class element
+        //click on search clears navbar except for magnify9ing class button and puts in a blinking cursor/placeholder thing
+        //types liket he fallout terminal
+
+
+        
